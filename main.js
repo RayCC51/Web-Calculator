e6aeb023dd5d28097bef8ceeffadd2944ae66db2
raycc51 <raycc51@gmail.com>
2025-02-08 21:33:57 +0900

    feat: start making key validation

--- main.js
+++ main.js
@@ -1,37 +1,55 @@
 const symbol = {
-  "num-one": "1",
-  "num-two": "2",
-  "num-three": "3",
-  "num-four": "4",
-  "num-five": "5",
-  "num-six": "6",
-  "num-seven": "7",
-  "num-eight": "8",
-  "num-nine": "9",
-  "num-zero": "0",
-  "num-thousand": "000",
-  "num-dot": ".",
-  "num-pi": "π",
-  "num-e": "e",
-  "op-parentheses": "()",
-  "op-log": "log",
-  "op-plus": "+",
-  "op-minus": "-",
-  "op-multiple": "*",
-  "op-divide": "/",
-  "op-power": "^",
-  "op-mod": "%",
-  "op-equal": "=",
+  "num-one": ["1", ],
+  "num-two": ["2", ],
+  "num-three": ["3", ],
+  "num-four": ["4", ],
+  "num-five": ["5", ],
+  "num-six": ["6", ],
+  "num-seven": ["7", ],
+  "num-eight": ["8", ],
+  "num-nine": ["9", ],
+  "num-zero": ["0", ],
+  "num-thousand": ["000", ],
+  "num-dot": [".", ],
+  "num-pi": ["π", ],
+  "num-e": ["e", ],
+  "op-parentheses": ["()", ],
+  "op-log": ["log", ],
+  "op-plus": ["+", ],
+  "op-minus": ["-", ],
+  "op-multiple": ["*", ],
+  "op-divide": ["/", ],
+  "op-power": ["^", ],
+  "op-mod": ["%", ],
+  "equal": ["=", ],
 };
+// thousand -> zero zero zero
+// parentheses -> open close
 
 // result
 const resultBox = document.getElementById("result");
+let resultArr = [];
 
 // change result value
 const changeResultValue = (item) => {
   let newKey = item.id;
-  resultBox.textContent = symbol[newKey];
+  if (true) {
+    resultArr.push(newKey);
+  } else{
+    resultArr.pop();
+    resultArr.push(newKey);
+  }
+  setResultValue();
 };
+
+// true will append new key, false will replace last key
+const isNewKeyValid = (key) => {
+}
+
+// set result list to string
+const setResultValue = () => {
+  resultBox.textContent = resultArr.join('');
+}
 
 
 // on button click
@@ -42,8 +60,6 @@ allBtn.forEach(item => {
     item.onclick = () => console.log(item.id);
   }
   else {
-
-
     item.onclick = () => changeResultValue(item);
   }
 });
